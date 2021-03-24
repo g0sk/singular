68af3bf1833a3f6b0aa9049b93cd03c3aaafd4fb
g0sk <osantana41@gmail.com>
2021-03-24 15:47:13 +0000

    fix: babel config

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['module:react-native-dotenv'],
-    'module-resolver',
-    {
-      root: ['./src'],
-      extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-      alias: {
-        '@components': './src/components',
+    'module:react-native-dotenv',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
       },
-    },
+    ],
   ],
 };
