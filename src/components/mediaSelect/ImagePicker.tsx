7d70860bff5a4fc6c392c8f05f12f6c3de3e98fb
g0sk <osantana41@gmail.com>
2021-05-19 11:25:45 +0100

    feat: image-picker component

--- src/components/mediaSelect/ImagePicker.tsx
+++ src/components/mediaSelect/ImagePicker.tsx
@@ -1,36 +1,204 @@
-import React, {SetStateAction, Dispatch} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {ImagePickerModal} from './Modal';
+import React, {Dispatch, useState, useEffect, SetStateAction} from 'react';
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ToastAndroid,
+  View,
+} from 'react-native';
+import {
+  ErrorCode,
+  launchImageLibrary,
+  ImagePickerResponse,
+  ImageLibraryOptions,
+  CameraOptions,
+  launchCamera,
+} from 'react-native-image-picker';
+
+type Error = {
+  errorCode: ErrorCode | undefined;
+  errorMessage: string | undefined;
+};
 
 export interface PermissionTypes {
   camera: boolean;
   storage: boolean;
 }
+
+export type ParsedImage = {
+  base64: string | undefined;
+  uri: string | undefined;
+  width: number | undefined;
+  height: number | undefined;
+  fileSize: number | undefined;
+  fileName: string | undefined;
+};
+
 type ImagePickerProps = {
   visible: boolean;
   setModalVisibility: Dispatch<SetStateAction<boolean>>;
+  saveImage: Dispatch<SetStateAction<ParsedImage | undefined>>;
 };
 
 export const ImagePicker: React.FC<ImagePickerProps> = ({
   visible,
   setModalVisibility,
+  saveImage,
 }) => {
+  const [isVisible, setIsVisible] = useState<boolean>(visible);
+
+  useEffect(() => {
+    setIsVisible(visible);
+  }, [visible]);
+
+  const parseImageResponse = (response: ImagePickerResponse): ParsedImage => {
+    //const base64URI = 'data:image/jpg;base64,' + response.base64;
+    return {
+      base64: response.base64,
+      uri: response.uri,
+      height: response.height,
+      width: response.width,
+      fileSize: response.fileSize,
+      fileName: response.fileName,
+    };
+  };
+
+  const errorToast = (errorMessage: string) => {
+    ToastAndroid.showWithGravity(
+      errorMessage,
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+    );
+  };
+
+  const hideModal = () => {
+    setIsVisible(false);
+    setModalVisibility(false);
+  };
+
+  const showGallery = () => {
+    const libraryOptions: ImageLibraryOptions = {
+      mediaType: 'photo',
+      includeBase64: true,
+      //maxWidth: 100,
+      //maxHeight: 100,
+      quality: 1,
+    };
+    launchImageLibrary(libraryOptions, (response: ImagePickerResponse) => {
+      if (response.errorCode) {
+        hideModal();
+        errorToast(response.errorCode);
+      }
+      if (!response.didCancel) {
+        hideModal();
+        const parsedImage = parseImageResponse(response);
+        saveImage(parsedImage);
+      }
+      return;
+    });
+  };
+  const showCamera = () => {
+    const cameraOptions: CameraOptions = {
+      mediaType: 'photo',
+      quality: 1,
+      cameraType: 'back',
+      includeBase64: true,
+      saveToPhotos: true,
+    };
+    launchCamera(cameraOptions, (response: ImagePickerResponse) => {
+      if (response.errorCode) {
+        hideModal();
+        errorToast(response.errorCode);
+      }
+      if (!response.didCancel) {
+        hideModal();
+        const parsedImage = parseImageResponse(response);
+        saveImage(parsedImage);
+      }
+      return;
+    });
+  };
+
   return (
     <View style={styles.centeredView}>
-      <ImagePickerModal
-        setModalVisibility={setModalVisibility}
-        visible={visible}
-      />
+      <Modal transparent={true} visible={isVisible}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={styles.header}>
+              <Text style={styles.textHeader}>Select Image</Text>
+            </View>
+            <View style={styles.options}>
+              <TouchableOpacity onPress={() => showCamera()}>
+                <Text style={styles.textOption}>Take picture</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => showGallery()}>
+                <Text style={styles.textOption}>Choose from gallery</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.button}>
+              <TouchableOpacity
+                onPress={() => {
+                  hideModal();
+                }}>
+                <Text style={styles.cancelButton}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   centeredView: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 22,
     textAlign: 'left',
+  },
+  modalView: {
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    borderRadius: 20,
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    textAlign: 'left',
+  },
+  header: {
+    paddingVertical: 20,
+    paddingLeft: 20,
+    paddingRight: 70,
+    marginBottom: 20,
+  },
+  textHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  options: {
+    paddingLeft: 20,
+    paddingRight: 70,
+    marginBottom: 20,
+  },
+  textOption: {
+    marginBottom: 30,
+    fontSize: 15,
+  },
+  button: {
+    marginBottom: 20,
+    paddingHorizontal: 20,
+  },
+  cancelButton: {
+    textAlign: 'right',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
   },
 });
--- src/screens/Document/index.tsx
+++ src/screens/Document/index.tsx
@@ -1,14 +1,32 @@
 import React, {useState} from 'react';
-import {View, Text, Button} from 'react-native';
-import {ImagePicker} from 'components/mediaSelect/ImagePicker';
+import {View, Text, Button, Image, StyleSheet} from 'react-native';
+import {ImagePicker, ParsedImage} from 'components/mediaSelect/ImagePicker';
 
 export const Document = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [image, setImage] = useState<ParsedImage>();
+
   return (
     <View>
       <Text> Document view</Text>
       <Button title="Select image" onPress={() => setShowModal(true)} />
-      <ImagePicker visible={showModal} setModalVisibility={setShowModal} />
+      <ImagePicker
+        visible={showModal}
+        setModalVisibility={setShowModal}
+        saveImage={setImage}
+      />
+      {image?.uri !== undefined ? (
+        <Image style={styles.image} source={{uri: image.uri}} />
+      ) : (
+        <Text>No photo </Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    height: 100,
+    width: 100,
+  },
+});
